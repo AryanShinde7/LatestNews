4abf7e01d9e51467742e8bfc3065173c95fccef3
Your Name <aryanshinde.5105@gmail.com>
2024-12-07 14:14:56 +0530

    build folder deleted

--- src/Components/NewsBoard.jsx
+++ src/Components/NewsBoard.jsx
@@ -7,7 +7,7 @@ function NewsBoard({category}) {
 
   useEffect(() => {
     let url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=345c3759336e477883a110937e10c1ca`
-    fetch(url).then(response => response.json()).then(data => setarticles(data.articles))
+    fetch(url).then(response => response.json()).then(data => setarticles(data.articles)).catch(err => console.log(err))
   }, [category]);
 
   return (
@@ -16,9 +16,9 @@ function NewsBoard({category}) {
       <h2 className="text-center">
         LATEST <span className="badge bg-danger">NEWS</span>
       </h2>
-     {articles.map((news , index) =>{
+     {articles?(articles.map((news , index) =>{
         return <NewsItems key={index} title={news.title} description={news.description} src={news.urlToImage} url={news.url}/>
-    })}
+    })):<p>LOADING .....</p>}
     </div>
   );
 }
